b9c53377bc6101a99800da945b2043a4c4c3822f
fire855 <TheFire855@gmail.com>
2014-12-29 02:53:55 +0100

    update

--- omni_sun4i.mk
+++ omni_sun4i.mk
@@ -17,6 +17,6 @@ PRODUCT_MANUFACTURER := yftech
 PRODUCT_COPY_FILES += \
 device/yftech/sun4i/sun4i-ts.ko:recovery/root/sun4i-ts.ko \
 device/yftech/sun4i/ueventd.sun4i.rc:recovery/root/ueventd.sun4i.rc \
-device/yftech/sun4i/init.sun4i.usb.rc:recovery/root/init.sun4i.usb.rc \
-device/yftech/sun4i/init.recovery.sun4i.rc:recovery/root/init.recovery.sun4i.rc
+device/yftech/sun4i/init.sun4i.usb.rc:recovery/root/usb.rc \
+device/yftech/sun4i/init.recovery.sun4i.rc:root/init.recovery.sun4i.rc
 
